b1a57369495901e59f95549420c9f5332b1fe7a5
xwei4 <xwei4@oberlin.edu>
2025-12-06 17:46:12 -0500

    code

--- code.py
+++ code.py
@@ -2,5 +2,11 @@ import gurobipy as gp
 from gurobipy import GRB
 import pandas as pd
 
-df = pd.read_csv("test.txt")
+df = pd.read_csv("test.cvs")
 
+foods = df["Food_Item"].tolist()
+
+m = gp.Model("diet_opt")
+x = m.addVars(foods, name="x", lb=0, vtype=GRB.CONTINUOUS)
+
+m.setObjective(gp.quicksum(df.loc[i, "Estimated_Price (USD)"] * x[foods[i]] for i in range(len(foods))),GRB.MINIMIZE)
