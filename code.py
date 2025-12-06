19cf80c41317b9566e30ef5c23c274ca62d51983
xwei4 <xwei4@oberlin.edu>
2025-12-06 17:49:12 -0500

    code

--- code.py
+++ code.py
@@ -10,3 +10,10 @@ m = gp.Model("diet_opt")
 x = m.addVars(foods, name="x", lb=0, vtype=GRB.CONTINUOUS)
 
 m.setObjective(gp.quicksum(df.loc[i, "Estimated_Price (USD)"] * x[foods[i]] for i in range(len(foods))),GRB.MINIMIZE)
+
+MAX_SUGARS   = 50    
+MAX_CHOL     = 300  
+MIN_FIBER    = 28   
+MIN_PROTEIN  = 50   
+MAX_FAT      = 78   
+MIN_FAT      = 20    
